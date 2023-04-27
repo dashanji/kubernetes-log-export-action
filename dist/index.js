f12e233ccdd0d60f0932d59864573bddde112823
Ye Cao <caoye.cao@alibaba-inc.com>
2023-04-27 19:53:04 +0800

    test
    Signed-off-by: Ye Cao <caoye.cao@alibaba-inc.com>

--- dist/index.js
+++ dist/index.js
@@ -4188,8 +4188,11 @@ function getFileTree(dir, fileTree = { name: dir, type: 'directory', children: [
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const showTimestamps = Boolean(_actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('show_timestamps'));
-            const outputDir = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('output_dir'); // ./output-minikube-manusa
+            //const showTimestamps = Boolean(_actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('show_timestamps'));
+            //const outputDir = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('output_dir'); // ./output-minikube-manusa
+            const showTimestamps = Boolean(process.env.SHOW_TIMESTAMPS);
+            const outputDir = process.env.OUTPUT_DIR;
+            _actions_core__WEBPACK_IMPORTED_MODULE_0__.get
             _actions_core__WEBPACK_IMPORTED_MODULE_0__.info('Running kubectl scripts...');
             _actions_core__WEBPACK_IMPORTED_MODULE_0__.info(`Dumping logs for all namespaces...`);
             _actions_exec__WEBPACK_IMPORTED_MODULE_1__.exec('./kubelogs.sh', ['-o', outputDir, '--timestamps', String(showTimestamps)]);
