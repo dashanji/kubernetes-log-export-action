c9986328489d15820ae7b7e8762764573028401d
Ye Cao <caoye.cao@alibaba-inc.com>
2023-05-25 16:39:00 +0800

    fix bugs
    Signed-off-by: Ye Cao <caoye.cao@alibaba-inc.com>

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Print the values of the arguments
+echo "namespaces: $1"
+echo "arg2: $2"
+echo "arg3: $3"
+
 # Start the Kubernetes Log Export Action daemon process
 echo "Starting the Kubernetes Log Export Action daemon process..."
 echo "To stop the daemon process, run ./stop.sh"
@@ -7,6 +12,6 @@ echo "To stop the daemon process, run ./stop.sh"
 echo "Running kubelogs.sh on namespaces: $NAMESPACES, and outputting to $OUTPUT_DIR."
 while true
 do
-    bash kubelogs.sh -n "$NAMESPACES" -o $OUTPUT_DIR --timestamps $SHOW_TIMESTAMPS
+    bash kubelogs.sh -n $1 -o $2 --timestamps $3
     sleep 1
 done
